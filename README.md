db7345021c2da28c686d1ef52bca1b37aef3a601
Jan Hacker <jan.hacker@competec.ch>
2020-05-08 00:35:22 +0200

    "import"/copy-pasta https://github.com/aarushik93/webcam-go

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ go? or rust wasm suggester - how small can it get? no decompile?
 experiment: hide some "valuable" (i.e. andre's) code in a (how?) small wasm bin?
 
 - https://github.com/golang/go/wiki/WebAssembly
+
+## drift-off experiment
+
+https://github.com/makiuchi-d/gozxing looks more wasm-able (pure go) than ...
+- https://github.com/bieber/barcode
+- https://github.com/unicod3/realtime-barcode-detector
+
++
+
+https://github.com/aarushik93/webcam-go
--- main.go
+++ main.go
@@ -3,14 +3,25 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+
+	"github.com/schnoddelbotz/suggest-wasm/filter"
 )
 
 func main() {
 	fmt.Println("Hello, WebAssembly!")
 	c := make(chan struct{}, 0)
 
-	println("WASM Go Initialized")
+	println("WASM Go Initializing...")
+
+	// register suggest func here to run when JS suggest function is called
 	js.Global().Set("suggest", js.FuncOf(suggest))
+
+	// webcam filter
+	applyFilterFunc := js.FuncOf(applyFilter) // wrapper function
+	js.Global().Set("applyFilter", applyFilterFunc)
+	defer applyFilterFunc.Release()
+
+
 	<-c
 }
 
@@ -18,6 +29,37 @@ func main() {
 type NewUser struct {
 	Email    string
 	Password string
+}
+
+var uint8Array = js.Global().Get("Uint8Array")
+var threshold = 127
+
+func applyFilter(this js.Value, args []js.Value) interface{} {
+
+	val := args[0].Int()
+	length := args[1].Length()
+
+	jsPixels := make(filter.Pixels, length)
+
+	_ = js.CopyBytesToGo(jsPixels, args[1])
+
+	if val == 0 {
+		jsPixels.MakeGrey()
+
+	} else if val == 1 {
+		jsPixels.Invert()
+
+	} else if val == 2 {
+		jsPixels.MakeNoise()
+
+	} else if val == 3 {
+		jsPixels.MakeRed()
+	}
+
+	buf := uint8Array.New(len(jsPixels))
+	js.CopyBytesToJS(buf, jsPixels)
+
+	return buf
 }
 
 func suggest(t js.Value, tt []js.Value) interface{} {
